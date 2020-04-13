08525c5a4596a13106c32db084720a3413124946
VIENS25 <60590172+VIENS25@users.noreply.github.com>
2020-04-13 21:32:26 +0700

    Add files via upload

---
+++ Resto.php.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">  
+    <link rel="stylesheet" href = "a.css">
+    <title>MENU</title>
+</head>
+<body class>
+    
+    <div class = "atas">
+      <img src="tsuki.png" alt="ikon" width="257.4vw" height="89.2vw" style="margin-left: 3vw;margin-top:2vw;">
+        <p style="margin-top:1.5vw; margin-left:3vw;font-family:san-serif;font-size:1.5vw">
+          This my first html "Restoran TSUKI ". In this first project I made a restaurant project.
+        </p>
+    </div>
+
+    <div class="n" style="width: 19vw;height: 22vw;">
+      <img src="nasgor2.jpg" class="foto">
+       <h3 class="h3">Nasi Goreng</h3>
+       <p class="p"> Harga = 17000</p>
+    </div>
+
+    <div class="n" style="width: 19vw;height: 22vw;">
+      <img src="bakso2.jpg" class="foto">
+       <h3 class="h3">Bakso Rudal</h3>
+       <p class="p"> Harga = 15000</p>
+   </div>
+
+   
+   <div style="float: left;margin-left: 20px;font-size: 18px;margin-top: 20px;">
+     <p>Menyediakan berbagai menu makanan. Restoran ini berdiri sejak saya mulai membuat web ini </p><br>
+     <p>dan allhamdulilahnya belum dibangun sama sekali. Saya bersukur bisa membuat web ini  </p><br>
+     <p>karena merupakan salah aspek penilaian dalam pembelajaran saya yang sangat mempengaruhi</p><br> 
+     <p>nilai saya di raport pada masa depan yang menentukan dimana saya berada dan bekerja.</p>
+
+   </div>
+</body>
+</html>
